f4a363fe8c87a0db9234b986336f07304125035e
gramaguru <gramaguru@salesforce.com>
2025-01-06 01:27:14 +0530

    Frontend update link

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/apidocs", {
+      const response = await fetch("https://bitter-glynda-guru22-fabbb125.koyeb.app/apidocs", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: input }),
